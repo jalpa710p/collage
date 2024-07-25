872e836dc6f105d089132759809d8a1ef21b95c0
jalpa710p <parmaryash201@gmail.com>
2024-07-25 14:45:00 +0530

    collage update

--- collageForm/settings.py
+++ collageForm/settings.py
@@ -91,6 +91,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
